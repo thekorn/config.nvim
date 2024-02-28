664bcb52e5e65952f4647126757c59b9ea975451
Markus Korn <markus.korn@gmail.com>
2024-02-28 05:04:29 +0100

    🎨 style(init.lua): switch colorscheme from 'tokyonight-night' to 'onedark' for better readability and aesthetics

--- init.lua
+++ init.lua
@@ -725,12 +725,12 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'folke/tokyonight.nvim',
+    'navarasu/onedark.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'onedark'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
