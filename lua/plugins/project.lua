991087d14295d122b90a892a44c48a168cc36a68
Markus Korn <markus.korn@gmail.com>
2025-04-06 18:42:43 +0200

    Refactor project management and session handling
    - Switch from `project.nvim` to `neovim-project` for project management, updating configurations and dependencies accordingly.
    - Add `neovim-session-manager` to manage and persist session states.
    - Update `telescope.nvim` dependency to the latest commit for enhanced functionality.

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,40 +1,39 @@
 return {
-    "ahmedkhalf/project.nvim",
+    "coffebar/neovim-project",
+    opts = {
+        projects = { -- define project roots
+            "~/devel/*/*/*",
+            "~/.config/nvim",
+        },
+        picker = {
+            type = "telescope", -- or "fzf-lua"
+            --type = "fzf-lua",
+            preview = {
+                enabled = false, -- show directory structure in Telescope preview
+                git_status = true, -- show branch name, an ahead/behind counter, and the git status of each file/folder
+                show_hidden = true, -- show hidden files/folders
+            },
+        },
+    },
     init = function()
-        require("project_nvim").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-
-
-        -- Absolutely minimal implementation of fzf-lua based project finder
-        -- for fzf-lua, due to request from @KrisWilliams1 (Maybe extended to a
-        -- full blown port from the original selector in the future)
-        local history = require("project_nvim.utils.history")
-        local project = require("project_nvim.project")
-
-        vim.api.nvim_create_user_command("FzfProjects", function()
-            local projects = history.get_recent_projects()
-
-            require("fzf-lua").fzf_exec(projects, {
-                prompt = "Projects> ",
-                actions = {
-                    ["default"] = function(selected)
-                        if selected and #selected > 0 then
-                            local project_path = selected[1]
-                            if project.set_pwd(project_path, "fzf-lua") then
-                                require("fzf-lua").files()
-                            end
-                        end
-                    end
-                }
-            })
-        end, {})
+        -- enable saving the state of plugins in the session
+        vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
     end,
+    dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        -- optional picker
+        { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+        -- optional picker
+        --{ "ibhagwan/fzf-lua" },
+        { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
     keys = {
         {
-            "<leader>fp", "<cmd>FzfProjects<CR>", desc="Find Recent Projects"
-        }
-    }
+            "<leader>fp",
+            "<cmd>NeovimProjectHistory<CR>",
+            desc = "Find Recent Projects",
+        },
+    },
 }
