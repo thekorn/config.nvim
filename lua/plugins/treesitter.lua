f019f57a54c54ea76c9e3dcaa5244c2b6c0b176d
Markus Korn <markus.korn@gmail.com>
2025-06-20 19:03:28 +0200

    Refactor Treesitter configuration to simplify setup
    - Streamline `nvim-treesitter` setup by keeping only essential configuration elements.
    - Focus on simplifying the plugin maintenance and reducing complexity in `treesitter.lua`.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,6 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    branch = "main", --"master",
-    lazy = false,
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
-            ensure_installed = {
-                "lua",
-                "vimdoc",
-                "query",
-                "typescript",
-                "javascript",
-                "html",
-                "markdown",
-                "markdown_inline",
-                "css",
-                "dart",
-                "nix",
-                "zig",
-                "gitcommit",
-            },
-            auto_install = true,
-            sync_install = false,
-            ignore_install = {},
-            highlight = { enable = true },
-            indent = { enable = true },
-
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<Enter>", -- set to `false` to disable one of the mappings
-                    node_incremental = "<Enter>",
-                    scope_incremental = false,
-                    node_decremental = "<Backspace>",
-                },
-            },
-        })
-    end,
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
 }
