a1652a66ec65d3dd4d66505b229d366c665acea7
Markus Korn <markus.korn@gmail.com>
2025-07-30 18:59:05 +0200

    fix copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,20 +4,7 @@ return {
     build = ":Copilot auth",
     event = "BufReadPost",
     opts = {
-        suggestion = {
-            enabled = not vim.g.ai_cmp,
-            auto_trigger = true,
-            hide_during_completion = vim.g.ai_cmp,
-            keymap = {
-                accept = false, -- handled by nvim-cmp / blink.cmp
-                next = "<M-]>",
-                prev = "<M-[>",
-            },
-        },
+        suggestion = { enabled = false },
         panel = { enabled = false },
-        filetypes = {
-            markdown = true,
-            help = true,
-        },
     },
 }
--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -1,6 +1,7 @@
 return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
+        enabled = false,
         dependencies = {
             { "zbirenbaum/copilot.lua" },
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
